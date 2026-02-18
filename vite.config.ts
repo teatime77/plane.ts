4d10211cc5f8463478ee94831cb5cec394745a80
teatime77 <teatime77@live.jp>
2026-02-18 09:56:09 +0900

    outDir => ./dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig, mergeConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { baseConfig } from '../vite.config.base';
 import { resolve } from 'path';
 
 export default defineConfig(
   mergeConfig(baseConfig, {
+    plugins: [tsconfigPaths()],
     build: {
       minify: false,
       lib: {
         entry: resolve(__dirname, 'ts/index.ts'),
-        fileName: 'index',
+        fileName: 'plane',
         formats: ['es']
       },
-      outDir: './plane.ts/public/lib/plane'
+      outDir: '../dist'
     }
   })
 );
