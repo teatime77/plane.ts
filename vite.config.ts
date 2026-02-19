f34f696a5411074c885e18d6c087c56e475695f4
teatime77 <teatime77@live.jp>
2026-02-19 13:01:52 +0900

    outDir => ./lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
+// root/webgpu.ts/vite.config.ts
 import { defineConfig, mergeConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { baseConfig } from '../vite.config.base';
 import { resolve } from 'path';
 
 export default defineConfig(
   mergeConfig(baseConfig, {
-    plugins: [tsconfigPaths()],
     build: {
-      minify: false,
-      lib: {
-        entry: resolve(__dirname, 'ts/index.ts'),
-        fileName: 'plane',
-        formats: ['es']
-      },
-      outDir: '../dist/plane/assets'
+      // 各プロジェクト固有の出力先
+      outDir: '../dist/plane',
+      rollupOptions: {
+        input: {
+          // 各プロジェクト固有のエントリーポイント
+          main: resolve(__dirname, 'index.html'),
+        }
+      }
     }
   })
 );
